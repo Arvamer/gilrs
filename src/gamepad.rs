b7cf737f9d2a36f1acad98b96b26c58ce0caffa4
Arvamer <arvamer@gmail.com>
2016-06-27 13:06:18 +0200

    Chanege linux input consts type to u16

--- src/gamepad.rs
+++ src/gamepad.rs
@@ -79,17 +79,17 @@ impl<'a> Iterator for EventIterator<'a> {
             unimplemented!()
         } else {
             let event = unsafe { mem::transmute::<_, ioctl::input_event>(buff) };
-            if event._type as u32 == EV_KEY {
-                Button::from_u32(event.code as u32).and_then(|btn| {
+            if event._type == EV_KEY {
+                Button::from_u16(event.code).and_then(|btn| {
                     match event.value {
                         0 => Some(Event::ButtonReleased(btn)),
                         1 => Some(Event::ButtonPressed(btn)),
                         _ => None,
                     }
                 })
-            } else if event._type as u32 == EV_ABS {
+            } else if event._type == EV_ABS {
                 println!("Axis: {}", event.code);
-                Axis::from_u32(event.code as u32).map(|axis| {
+                Axis::from_u16(event.code).map(|axis| {
                     let val = event.value as f32;
                     let val = match axis {
                         Axis::LeftStickX => val / self.1.abs_x_max,
@@ -117,7 +117,7 @@ pub enum Event {
     AxisChanged(Axis, f32),
 }
 
-#[repr(u32)]
+#[repr(u16)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Button {
     // Action Pad
@@ -147,7 +147,7 @@ pub enum Button {
 }
 
 impl Button {
-    fn from_u32(btn: u32) -> Option<Self> {
+    fn from_u16(btn: u16) -> Option<Self> {
         if btn >= BTN_SOUTH && btn <= BTN_THUMBR || btn >= BTN_DPAD_UP && btn <= BTN_DPAD_RIGHT {
             Some(unsafe { mem::transmute(btn) })
         } else {
@@ -156,7 +156,7 @@ impl Button {
     }
 }
 
-#[repr(u32)]
+#[repr(u16)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Axis {
     LeftStickX = ABS_X,
@@ -170,7 +170,7 @@ pub enum Axis {
 }
 
 impl Axis {
-    fn from_u32(axis: u32) -> Option<Self> {
+    fn from_u16(axis: u16) -> Option<Self> {
         if axis == ABS_X || axis == ABS_Y || axis == ABS_RX || axis == ABS_RY ||
            axis >= ABS_HAT1X && axis <= ABS_HAT2Y {
             Some(unsafe { mem::transmute(axis) })
@@ -191,7 +191,7 @@ fn open_and_check(path: &CStr) -> Option<Gamepad> {
         let mut key_bits = [0u8; KEY_MAX as usize];
 
         if ioctl::eviocgbit(fd, 0, EV_MAX as i32, ev_bits.as_mut_ptr()) < 0 ||
-           ioctl::eviocgbit(fd, EV_KEY, KEY_MAX as i32, key_bits.as_mut_ptr()) < 0 {
+           ioctl::eviocgbit(fd, EV_KEY as u32, KEY_MAX as i32, key_bits.as_mut_ptr()) < 0 {
             c::close(fd);
             return None;
         }
@@ -209,35 +209,35 @@ fn open_and_check(path: &CStr) -> Option<Gamepad> {
 
         let mut absi = ioctl::input_absinfo::default();
 
-        if ioctl::eviocgabs(fd, ABS_X, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_X as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_x_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_Y, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_Y as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_y_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_RX, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_RX as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_rx_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_RY, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_RY as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_ry_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_HAT1X, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_HAT1X as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_right_tr_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_HAT1Y, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_HAT1Y as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_left_tr_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_HAT2X, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_HAT2X as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_right_tr2_max = absi.maximum as f32;
         }
 
-        if ioctl::eviocgabs(fd, ABS_HAT2Y, &mut absi as *mut _) >= 0 {
+        if ioctl::eviocgabs(fd, ABS_HAT2Y as u32, &mut absi as *mut _) >= 0 {
             gamepad.axes_info.abs_left_tr2_max = absi.maximum as f32;
         }
 
@@ -248,42 +248,42 @@ fn open_and_check(path: &CStr) -> Option<Gamepad> {
     }
 }
 
-fn test_bit(n: u32, array: &[u8]) -> bool {
+fn test_bit(n: u16, array: &[u8]) -> bool {
     (array[(n / 8) as usize] >> (n % 8)) & 1 != 0
 }
 
-const KEY_MAX: u32 = 0x2ff;
-const EV_MAX: u32 = 0x1f;
-const EV_KEY: u32 = 0x01;
-const EV_ABS: u32 = 0x03;
+const KEY_MAX: u16 = 0x2ff;
+const EV_MAX: u16 = 0x1f;
+const EV_KEY: u16 = 0x01;
+const EV_ABS: u16 = 0x03;
 
-const BTN_GAMEPAD: u32 = 0x130;
-const BTN_SOUTH: u32 = 0x130;
-const BTN_EAST: u32 = 0x131;
-const BTN_C: u32 = 0x132;
-const BTN_NORTH: u32 = 0x133;
-const BTN_WEST: u32 = 0x134;
-const BTN_Z: u32 = 0x135;
-const BTN_TL: u32 = 0x136;
-const BTN_TR: u32 = 0x137;
-const BTN_TL2: u32 = 0x138;
-const BTN_TR2: u32 = 0x139;
-const BTN_SELECT: u32 = 0x13a;
-const BTN_START: u32 = 0x13b;
-const BTN_MODE: u32 = 0x13c;
-const BTN_THUMBL: u32 = 0x13d;
-const BTN_THUMBR: u32 = 0x13e;
+const BTN_GAMEPAD: u16 = 0x130;
+const BTN_SOUTH: u16 = 0x130;
+const BTN_EAST: u16 = 0x131;
+const BTN_C: u16 = 0x132;
+const BTN_NORTH: u16 = 0x133;
+const BTN_WEST: u16 = 0x134;
+const BTN_Z: u16 = 0x135;
+const BTN_TL: u16 = 0x136;
+const BTN_TR: u16 = 0x137;
+const BTN_TL2: u16 = 0x138;
+const BTN_TR2: u16 = 0x139;
+const BTN_SELECT: u16 = 0x13a;
+const BTN_START: u16 = 0x13b;
+const BTN_MODE: u16 = 0x13c;
+const BTN_THUMBL: u16 = 0x13d;
+const BTN_THUMBR: u16 = 0x13e;
 
-const BTN_DPAD_UP: u32 = 0x220;
-const BTN_DPAD_DOWN: u32 = 0x221;
-const BTN_DPAD_LEFT: u32 = 0x222;
-const BTN_DPAD_RIGHT: u32 = 0x223;
+const BTN_DPAD_UP: u16 = 0x220;
+const BTN_DPAD_DOWN: u16 = 0x221;
+const BTN_DPAD_LEFT: u16 = 0x222;
+const BTN_DPAD_RIGHT: u16 = 0x223;
 
-const ABS_X: u32 = 0x00;
-const ABS_Y: u32 = 0x01;
-const ABS_RX: u32 = 0x03;
-const ABS_RY: u32 = 0x04;
-const ABS_HAT1X: u32 = 0x12;
-const ABS_HAT1Y: u32 = 0x13;
-const ABS_HAT2X: u32 = 0x14;
-const ABS_HAT2Y: u32 = 0x15;
+const ABS_X: u16 = 0x00;
+const ABS_Y: u16 = 0x01;
+const ABS_RX: u16 = 0x03;
+const ABS_RY: u16 = 0x04;
+const ABS_HAT1X: u16 = 0x12;
+const ABS_HAT1Y: u16 = 0x13;
+const ABS_HAT2X: u16 = 0x14;
+const ABS_HAT2Y: u16 = 0x15;
