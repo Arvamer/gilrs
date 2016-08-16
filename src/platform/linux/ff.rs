e08ad9f8ca1730fd7687e353c51d825bb677d50d
Arvamer <arvamer@gmail.com>
2016-08-16 14:39:32 +0200

    Add license

--- src/platform/linux/ff.rs
+++ src/platform/linux/ff.rs
@@ -1,3 +1,9 @@
+// Copyright 2016 GilRs Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 use ff::{EffectData, Waveform, Trigger};
 use super::gamepad::Gamepad;
 use ioctl::{ff_effect, input_event};
--- src/platform/linux/gamepad.rs
+++ src/platform/linux/gamepad.rs
@@ -1,3 +1,9 @@
+// Copyright 2016 GilRs Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 use super::udev::*;
 use AsInner;
 use gamepad::{Event, Button, Axis, Status, Gamepad as MainGamepad, GamepadImplExt};
