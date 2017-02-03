f1291f179eca05007b15228780e71f4c7d18af14
Arvamer <arvamer@gmail.com>
2017-02-03 17:31:13 +0100

    Add missing license headers

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,9 @@
+// Copyright 2016 GilRs Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 pub fn apply_deadzone(x: f32, y: f32, threshold: f32) -> (f32, f32) {
     let magnitude = (x * x + y * y).sqrt();
     if magnitude <= threshold {
