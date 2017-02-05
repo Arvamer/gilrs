e3a38c8cbb552dc62c193cf02955de0853629d18
Arvamer <arvamer@gmail.com>
2017-02-05 13:37:46 +0100

    Add rumble effect to ff example

--- examples/ff.rs
+++ examples/ff.rs
@@ -4,31 +4,46 @@ extern crate env_logger;
 use gilrs::Gilrs;
 use gilrs::ff::{EffectData, EffectType, Waveform, Envelope, Replay};
 use std::time::Duration;
-use std::thread;
+use std::{thread, env};
 
 fn main() {
     env_logger::init().unwrap();
     let mut gilrs = Gilrs::new();
-    let effect = EffectData {
-        kind: EffectType::Periodic {
-            wave: Waveform::Sine,
-            period: 1000,
-            magnitude: 30_000,
-            offset: 0,
-            phase: 0,
-            envelope: Envelope {
-                attack_length: 2000,
-                attack_level: 0,
-                fade_length: 1000,
-                fade_level: 5000,
-            },
-        },
-        replay: Replay {
-            length: 5000,
-            delay: 0,
-        },
-        ..Default::default()
-    };
+    let effect =
+        if env::args().nth(1).and_then(|s| if s == "rumble" { Some(()) } else { None }).is_none() {
+            EffectData {
+                kind: EffectType::Periodic {
+                    wave: Waveform::Sine,
+                    period: 1000,
+                    magnitude: 30_000,
+                    offset: 0,
+                    phase: 0,
+                    envelope: Envelope {
+                        attack_length: 2000,
+                        attack_level: 0,
+                        fade_length: 1000,
+                        fade_level: 5000,
+                    },
+                },
+                replay: Replay {
+                    length: 5000,
+                    delay: 0,
+                },
+                ..Default::default()
+            }
+        } else {
+            EffectData {
+                replay: Replay {
+                    length: 5000,
+                    delay: 0,
+                },
+                kind: EffectType::Rumble {
+                    strong: 20_000,
+                    weak: 0,
+                },
+                ..Default::default()
+            }
+        };
 
     let effect_idx = gilrs[0].add_ff_effect(effect).unwrap();
     let _ = gilrs[0].ff_effect(effect_idx).unwrap().play(2);
