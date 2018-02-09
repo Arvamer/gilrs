8e35a05b4e81ca0182546643936c0b42e4a0f2db
Mateusz Sieczko <arvamer@gmail.com>
2018-02-09 16:20:44 +0100

    Update copyright year

--- src/platform/default/gamepad.rs
+++ src/platform/default/gamepad.rs
@@ -1,4 +1,4 @@
-// Copyright 2017 Mateusz Sieczko and other GilRs Developers
+// Copyright 2016-2018 Mateusz Sieczko and other GilRs Developers
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
