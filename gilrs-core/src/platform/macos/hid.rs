382a415305003b467fac1b9d20905fbc7ef4d570
Junji Takakura <j.takakura@gmail.com>
2018-11-07 06:50:21 +0900

    Add IOHIDDevice

--- gilrs-core/src/platform/macos/hid.rs
+++ gilrs-core/src/platform/macos/hid.rs
@@ -8,21 +8,26 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
-use core_foundation::array::CFArray;
-use core_foundation::base::{kCFAllocatorDefault, CFRelease, TCFType};
+use core_foundation::array::{
+    kCFTypeArrayCallBacks, CFArray, CFArrayAppendValue, CFArrayCreateMutable, CFArrayGetCount,
+    CFArrayGetValueAtIndex,
+};
+use core_foundation::base::{kCFAllocatorDefault, CFRelease, CFType, TCFType};
 use core_foundation::dictionary::CFDictionary;
 use core_foundation::number::CFNumber;
 use core_foundation::runloop::{CFRunLoop, CFRunLoopMode};
-use core_foundation::string::CFString;
+use core_foundation::set::CFSetApplyFunction;
+use core_foundation::string::{kCFStringEncodingUTF8, CFString, CFStringCreateWithCString};
 
-use io_kit_sys::hid::base::{IOHIDDeviceCallback, IOHIDValueCallback};
+use io_kit_sys::hid::base::{IOHIDDeviceCallback, IOHIDDeviceRef, IOHIDValueCallback};
+use io_kit_sys::hid::device::{IOHIDDeviceGetProperty, IOHIDDeviceGetTypeID};
 use io_kit_sys::hid::keys::*;
 use io_kit_sys::hid::manager::*;
 use io_kit_sys::hid::usage_tables::*;
 use io_kit_sys::ret::{kIOReturnSuccess, IOReturn};
 
 use std::ffi::CStr;
-use std::os::raw::c_void;
+use std::os::raw::{c_char, c_void};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -100,6 +105,44 @@ impl IOHIDManager {
     ) {
         unsafe { IOHIDManagerRegisterInputValueCallback(self.0, callback, context) }
     }
+
+    pub fn get_devices(&mut self) -> Vec<IOHIDDevice> {
+        let copied = unsafe { IOHIDManagerCopyDevices(self.0) };
+
+        if copied.is_null() {
+            return vec![];
+        }
+
+        let devices =
+            unsafe { CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks) };
+
+        if devices.is_null() {
+            unsafe { CFRelease(copied as _) };
+            return vec![];
+        }
+
+        unsafe { CFSetApplyFunction(copied, cf_set_applier, devices as _) };
+        unsafe { CFRelease(copied as _) };
+
+        let device_count = unsafe { CFArrayGetCount(devices) };
+        let mut vec = Vec::with_capacity(device_count as _);
+
+        for i in 0..device_count {
+            let device = unsafe { CFArrayGetValueAtIndex(devices, i) };
+
+            if device.is_null() {
+                continue;
+            }
+
+            if let Some(device) = IOHIDDevice::new(device as _) {
+                vec.push(device);
+            }
+        }
+
+        unsafe { CFRelease(devices as _) };
+
+        vec
+    }
 }
 
 impl Drop for IOHIDManager {
@@ -111,6 +154,158 @@ impl Drop for IOHIDManager {
 unsafe impl Send for IOHIDManager {}
 unsafe impl Sync for IOHIDManager {}
 
+#[repr(C)]
+#[derive(Debug)]
+pub struct IOHIDDevice(IOHIDDeviceRef);
+
+impl_TCFType!(IOHIDDevice, IOHIDDeviceRef, IOHIDDeviceGetTypeID);
+
+impl IOHIDDevice {
+    pub fn new(device: IOHIDDeviceRef) -> Option<IOHIDDevice> {
+        if device.is_null() {
+            None
+        } else {
+            Some(IOHIDDevice(device))
+        }
+    }
+
+    pub fn get_name(&self) -> Option<String> {
+        match self.get_string_property(kIOHIDProductKey) {
+            Some(name) => Some(name.to_string()),
+            None => None,
+        }
+    }
+
+    pub fn get_location_id(&self) -> Option<u32> {
+        match self.get_number_property(kIOHIDLocationIDKey) {
+            Some(location_id) => match location_id.to_i32() {
+                Some(location_id) => Some(location_id as u32),
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn get_bustype(&self) -> Option<u16> {
+        match self.get_transport_key() {
+            Some(transport_key) => {
+                if transport_key == "USB".to_string() {
+                    Some(0x03)
+                } else if transport_key == "Bluetooth".to_string() {
+                    Some(0x05)
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    pub fn get_transport_key(&self) -> Option<String> {
+        match self.get_string_property(kIOHIDTransportKey) {
+            Some(transport_key) => Some(transport_key.to_string()),
+            None => None,
+        }
+    }
+
+    pub fn get_vendor_id(&self) -> Option<u16> {
+        match self.get_number_property(kIOHIDVendorIDKey) {
+            Some(vendor_id) => match vendor_id.to_i32() {
+                Some(vendor_id) => Some(vendor_id as u16),
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn get_product_id(&self) -> Option<u16> {
+        match self.get_number_property(kIOHIDProductIDKey) {
+            Some(product_id) => match product_id.to_i32() {
+                Some(product_id) => Some(product_id as u16),
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn get_version(&self) -> Option<u16> {
+        match self.get_number_property(kIOHIDVersionNumberKey) {
+            Some(version) => match version.to_i32() {
+                Some(version) => Some(version as u16),
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn get_page(&self) -> Option<u32> {
+        match self.get_number_property(kIOHIDPrimaryUsagePageKey) {
+            Some(page) => match page.to_i32() {
+                Some(page) => Some(page as u32),
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn get_usage(&self) -> Option<u32> {
+        match self.get_number_property(kIOHIDPrimaryUsageKey) {
+            Some(usage) => match usage.to_i32() {
+                Some(usage) => Some(usage as u32),
+                None => None,
+            },
+            None => None,
+        }
+    }
+}
+
+impl Properties for IOHIDDevice {
+    fn get_property(&self, key: *const c_char) -> Option<CFType> {
+        let key =
+            unsafe { CFStringCreateWithCString(kCFAllocatorDefault, key, kCFStringEncodingUTF8) };
+        let value = unsafe { IOHIDDeviceGetProperty(self.0, key) };
+
+        if value.is_null() {
+            None
+        } else {
+            Some(unsafe { TCFType::wrap_under_get_rule(value) })
+        }
+    }
+}
+
+unsafe impl Send for IOHIDDevice {}
+unsafe impl Sync for IOHIDDevice {}
+
+trait Properties {
+    fn get_string_property(&self, key: *const c_char) -> Option<CFString> {
+        match self.get_property(key) {
+            Some(value) => {
+                if value.instance_of::<CFString>() {
+                    Some(unsafe { CFString::wrap_under_get_rule(value.as_CFTypeRef() as _) })
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn get_number_property(&self, key: *const c_char) -> Option<CFNumber> {
+        match self.get_property(key) {
+            Some(value) => {
+                if value.instance_of::<CFNumber>() {
+                    Some(unsafe { CFNumber::wrap_under_get_rule(value.as_CFTypeRef() as _) })
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn get_property(&self, key: *const c_char) -> Option<CFType>;
+}
+
 fn create_hid_device_matcher(page: u32, usage: u32) -> CFDictionary<CFString, CFNumber> {
     let page_key = unsafe { CStr::from_ptr(kIOHIDDeviceUsagePageKey as _) };
     let page_key = CFString::from(page_key.to_str().unwrap());
@@ -121,4 +316,8 @@ fn create_hid_device_matcher(page: u32, usage: u32) -> CFDictionary<CFString, CF
     let usage_value = CFNumber::from(usage as i32);
 
     CFDictionary::from_CFType_pairs(&[(page_key, page_value), (usage_key, usage_value)])
+}
+
+extern "C" fn cf_set_applier(value: *const c_void, context: *const c_void) {
+    unsafe { CFArrayAppendValue(context as _, value) };
 }
