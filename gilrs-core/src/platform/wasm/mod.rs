123d5ff32d01b6a1857df961eaf8a44e9fecdefc
Ryan Goldstein <ryanisaacg@gmail.com>
2019-02-08 17:02:00 -0500

    Flip is reversed boolean

--- gilrs-core/src/platform/wasm/mod.rs
+++ gilrs-core/src/platform/wasm/mod.rs
@@ -4,4 +4,4 @@ mod gamepad;
 pub use self::ff::Device as FfDevice;
 pub use self::gamepad::{native_ev_codes, EvCode, Gamepad, Gilrs};
 
-pub const IS_Y_AXIS_REVERSED: bool = false;
+pub const IS_Y_AXIS_REVERSED: bool = true;
