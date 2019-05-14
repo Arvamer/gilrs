b168fc330143b1d7b22cba3e9a551f111fa05003
Mateusz Sieczko <arvamer@gmail.com>
2019-05-14 17:52:56 +0200

    Update env_logger and enable it for gilrs-core ev example

--- gilrs-core/examples/ev.rs
+++ gilrs-core/examples/ev.rs
@@ -1,8 +1,11 @@
 extern crate gilrs_core;
+extern crate env_logger;
 
 use gilrs_core::Gilrs;
 
 fn main() {
+    env_logger::init();
+
     let mut gilrs = Gilrs::new().unwrap();
     loop {
         while let Some(ev) = gilrs.next_event() {
